e5a4f1e196bc70d06dad2ff05c46d0050a65f177
zjxps2007 <zjxps2007@gmail.com>
2020-01-28 22:41:52 +0900

    Making_Discord_Bot

--- bot.py
+++ bot.py
@@ -2,11 +2,13 @@ import discord
 
 client = discord.Client()
 
-token = "NjcxNTg2Nzg3ODA5ODIwNzM0.Xi_GCg.WDTzsjGHcHYE7h9CHh2YVw2vgw8"
+token = ""
 
 @client.event
 async def on_ready():
     print('We have logged in as {0.user}'.format(client))
+    game = discord.Game("!커맨드")
+    await client.change_presence(status=discord.Status.online, activity=game)
 
 @client.event
 async def on_message(message):
@@ -14,5 +16,23 @@ async def on_message(message):
         return None
     if message.content == "!커맨드":
         await message.channel.send('Hello!')
+
+    if message.content.startswith("!등록"):
+        name = message.content[4:len(message.content)]
+        Id = client.user.id
+        await message.channel.send(Id)
+        try:
+            await message.channel.send(name)
+        except:
+            #print("다시 입력")
+            await message.channel.send("다시입력")
+
+
+
+
+# @client.evet
+# async def on_member_join(member):
+
+
 
 client.run(token)
