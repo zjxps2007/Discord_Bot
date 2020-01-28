67e63bdeb52dbe9d41457c97c20cc1f7c59ada53
zjxps2007 <zjxps2007@gmail.com>
2020-01-29 00:05:07 +0900

    Making_Discord_Bot

--- bot.py
+++ bot.py
@@ -7,8 +7,13 @@ token = ""
 @client.event
 async def on_ready():
     print('We have logged in as {0.user}'.format(client))
-    game = discord.Game("!커맨드")
+    game = discord.Game("!커맨드|")
     await client.change_presence(status=discord.Status.online, activity=game)
+
+# @client.event
+# async def on_member_join(member):
+#     ts = member.id
+#     return ts
 
 @client.event
 async def on_message(message):
@@ -18,20 +23,16 @@ async def on_message(message):
         await message.channel.send('Hello!')
 
     if message.content.startswith("!등록"):
+        #!등록 다음부터 저장하는 변수 name
         name = message.content[4:len(message.content)]
-        Id = client.user.id
-        await message.channel.send(Id)
+        #유저 아이디 저장할 변수 & 출력
+        member = client.user.id
+        await message.channel.send(member)
+        #이름을 잘못등록햇을때의 예외처리
         try:
             await message.channel.send(name)
         except:
-            #print("다시 입력")
             await message.channel.send("다시입력")
-
-
-
-
-# @client.evet
-# async def on_member_join(member):
 
 
 
